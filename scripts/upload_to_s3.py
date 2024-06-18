d39c2ca12c071417c5f1d17f40e5ca51638d97c5
Keetarp <pkapoor1001@gmail.com>
2024-06-18 04:10:02 -0400

    use asynchronous for uploading to s3

--- scripts/upload_to_s3.py
+++ scripts/upload_to_s3.py
@@ -1,55 +1,94 @@
-import boto3
-from botocore.exceptions import ClientError
+import asyncio
+import aioboto3
+import botocore.exceptions
+import aiohttp
 import os
 import json
 from dotenv import load_dotenv
 import requests
 from io import BytesIO
-from tqdm import tqdm
+from tqdm.asyncio import tqdm as atqdm
 
 load_dotenv()
+         
 
-def upload_data():
+async def upload_data():
+    # Retrieve AWS credentials for client
     bucket_name = os.getenv('AWS_BUCKET_NAME')
     region = os.getenv('AWS_REGION')
     access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
     secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-    s3_client = boto3.client(
+    # Access book data from file
+    with open('data/books.json', 'r') as file:
+        books = json.load(file)
+
+    # Create asynchronous clients for HTTP req and S3 client
+    async with aiohttp.ClientSession() as session, aioboto3.Session().client(
         's3',
         region_name=region,
         aws_access_key_id=access_key_id,
         aws_secret_access_key=secret_access_key
-    )
-
-    with open('data/books.json', 'r') as file:
-        books = json.load(file)
-        num_thumbnails_uploaded = 0
+    ) as s3_client:
         num_books = len(books)
 
-        for book in tqdm(books, total=num_books, desc="Uploading Book Thumnails to S3"):
+        # tasks will hold the coroutines to be executed
+        tasks = []
+
+        # progress bar
+        pbar = atqdm(total=num_books, desc="Uploading Book Thumbnails to S3")
+
+        # setup and start executing coroutines
+        for book in books:
             title = camel_case(book['title'])
             author = camel_case(book['author'])
             year = book['published_year']
-            thumbnail = book['thumbnail']
+            thumbnail_url = book['thumbnail']
             s3_key = f'{title}_{author}_{year}'
 
-            response = requests.get(thumbnail)
-            if response.status_code == 200:
-                image_data = BytesIO(response.content)
+            task = asyncio.create_task(download_and_upload(
+                session=session,
+                s3_client=s3_client,
+                thumbnail_url=thumbnail_url,
+                bucket_name=bucket_name,
+                s3_key=s3_key,
+                pbar=pbar
+            ))
+            tasks.append(task)
 
-                try:
-                    s3_client.upload_fileobj(image_data, bucket_name, s3_key)
-                    num_thumbnails_uploaded += 1
-                except ClientError as e:
-                    print(f"An AWS service error occurred: {e}")
-                except Exception as e:
-                    print(f"An error occurred: {e}")
-            else:
-                print(f'Failed to download image from {thumbnail}')
-    
+        # wait for coroutines to finish and count successes
+        results = await asyncio.gather(*tasks)
+        pbar.close()
+        num_thumbnails_uploaded = sum(results)
+
     print(f'Uploaded {num_thumbnails_uploaded}/{num_books} thumbails.')
 
+
+async def download_and_upload(session, s3_client, thumbnail_url, bucket_name, s3_key, pbar):
+    try:
+        async with session.get(thumbnail_url) as response:
+            if response.status == 200:
+                image_data = BytesIO(await response.read())
+                await s3_client.upload_fileobj(image_data, bucket_name, s3_key)
+                pbar.update(1)
+                return True
+            else:
+                print(f'Failed to download image from {thumbnail_url}. Status: {response.status}')
+                pbar.update(1)
+                return False
+            
+    except aiohttp.ClientError as e:
+        print(f'HTTP client error occured: {e}')
+        pbar.update(1)
+        return False
+    except botocore.exceptions.ClientError as e:
+        print(f"An AWS service error occurred: {e}")
+        pbar.update(1)
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        pbar.update(1)
+        return False
 
 def camel_case(text):
     words = text.split()
@@ -57,4 +96,4 @@ def camel_case(text):
 
 
 if __name__ == '__main__':
-    upload_data()
+    asyncio.run(upload_data())

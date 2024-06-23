0ed858a134dfbfa6460db04db334cc9f58ad8ebf
Keetarp <pkapoor1001@gmail.com>
2024-06-23 18:20:04 -0400

    code comments

--- scripts/generate_book_data.py
+++ scripts/generate_book_data.py
@@ -9,7 +9,15 @@ from dotenv import load_dotenv
 from tqdm import tqdm
 import time
 
-def main():
+def generate_book_data(file_path):
+    """
+    Generates book data by querying the Google Books API.
+    Uses concurrent fetching to improve performance and saves the fetched data to a JSON file.
+
+    Args:
+        file_path (str): The path to the JSON file to write data to.
+    """
+
     load_dotenv()
 
     # API settings
@@ -19,6 +27,7 @@ def main():
     categories = ["Romance", "Thriller", "Comics", "Mystery", "Action Adventure"]
     lengths = ["Short Read", "Standard Length", "Long Read"]
     formats = ["Ebook", "Audiobook", "Paperback"]
+    # Distribution weights for how published years should be assigned for each book
     published_year_bins = {
         'old': {
             'years': (1970, 1999),
@@ -38,14 +47,20 @@ def main():
     start = time.time()
 
     all_books = []
-    MAX_NUM_BOOKS = 100
+    MAX_NUM_BOOKS = 100 # Maximum number of books to generate data for
+    combinations = product(categories, lengths, formats)
+
+    # Used to keep track of previously processed books so there aren't duplicate title-author combos
+    # Lock is used to ensure thread-safe access to shared resources
     processed_books = set()
     lock = threading.Lock()
-    combinations = product(categories, lengths, formats)
     
+    # Concurrent data fetching
     with ThreadPoolExecutor(max_workers=10) as executor:
+        # Submit tasks to executor for fetching book data concurrently
         futures = [executor.submit(fetch_book_data, combo, api_url, published_year_bins, processed_books, lock) for combo in combinations]
 
+        # Process completed futures are they complete
         for future in tqdm(as_completed(futures), total=len(futures), desc="Generating Book Data"):
             if len(all_books) >= MAX_NUM_BOOKS:
                 break
@@ -54,20 +69,40 @@ def main():
             if books:
                 all_books.extend(books)
 
+    # Calculate elapsed time
     end = time.time()
     time_elapsed = end - start
     print(f'Time elapsed: {time_elapsed} seconds')
 
     print(f"Total books fetched: {len(all_books)}")
-    # Save JSON string to file
+    # Save book data to file as JSON
     with open('data/books.json', 'w') as file:
         json.dump(all_books, file, indent=4)
 
 def fetch_book_data(combination, api_url, published_year_bins, processed_books, lock):
+    """
+    Fetches book data from the Google Books API for a given combination of category, length, and format.
+    
+    Args:
+        combination (tuple): A combination of category, length, and format.
+        api_url (str): The URL of the Google Books API.
+        published_year_bins (dict): The distribution weights for published years.
+        processed_books (set): A set to keep track of processed books to avoid duplicates.
+        lock (threading.Lock): A lock to ensure thread-safe access to shared resources.
+    
+    Returns:
+        list: A list of processed books.
+    """
+
     category, length, book_format = combination
 
+    # Construct search query to search Google Books API
     query = f'Sci-Fi {category} {length} {book_format}'
+
+    # Fetch data
     results = query_google_books(query, api_url)
+
+    # Additional metadata to be included with the fetched data
     metadata = {
         'category': category,
         'length': length,
@@ -75,18 +110,31 @@ def fetch_book_data(combination, api_url, published_year_bins, processed_books, 
         'published_year_bins': published_year_bins
     }
 
+    # Process fetched data if there is any
     if results:
         return process_books(results, metadata, processed_books, lock)
     else:
         return []
 
-# Function to query Google Books API
+
 def query_google_books(query, api_url):
+    """
+    Queries the Google Books API with a given query string and API URL.
+    
+    Args:
+        query (str): The query string for the Google Books API.
+        api_url (str): The URL of the Google Books API.
+    
+    Returns:
+        dict or None: The JSON response from the API if successful, otherwise None.
+    """
+
     params = {
         'q': query,
         'maxResults': 10  # Limit results to top 10
     }
     
+    # Make request to Google Books API
     try:
         response = requests.get(url=api_url, params=params)
         response.raise_for_status()  # Raise an exception for bad requests
@@ -98,42 +146,62 @@ def query_google_books(query, api_url):
         print(f"An error occurred: {err}")
         return None
 
-# Function to process books
-def process_books(books_data, other_metadata, processed_books, lock):
-    books = []
-    MAX_NUM_BOOK_PER_COMBO = 2
 
+def process_books(books_data, other_metadata, processed_books, lock):
+    """
+    Processes book data from the Google Books API response, adding metadata and ensuring no duplicates.
+    
+    Args:
+        books_data (dict): The JSON response from the Google Books API.
+        other_metadata (dict): Additional metadata to be included with each book.
+        processed_books (set): A set to keep track of processed books to avoid duplicates.
+        lock (threading.Lock): A lock to ensure thread-safe access to shared resources.
+    
+    Returns:
+        list: A list of processed book entries.
+    """
+
+    books = []
+    MAX_NUM_BOOK_PER_COMBO = 2 # Maximum number of books to add for each combination of category, length, and book format
+
+    # Check if metadata was passed
     if not other_metadata:
         print('You forgot to pass in metadata for the books.')
         return books
 
+    # Process each item in response from Google Books API
     for item in books_data.get('items', []):
+        # Check if max number of books reached
         if len(books) >= MAX_NUM_BOOK_PER_COMBO:
             break
 
+        # Generate published year for book
         bins = [info["years"] for info in other_metadata["published_year_bins"].values()]
         bin_weights =[info["weight"] for info in other_metadata["published_year_bins"].values()]
         chosen_bin = random.choices(bins, weights=bin_weights, k=1)[0]
         published_year = random.randint(*chosen_bin)
 
+        # Extract data from response
         volume_info = item.get('volumeInfo', {})
         description = volume_info.get('description')
         thumbnail = volume_info.get('imageLinks', {}).get('thumbnail')
         title = volume_info.get('title')
 
+        # Ensure data includes an author; skip if missing
         authors = volume_info.get('authors')
         if not authors:
             continue
         author = authors[0]
 
+        # Ensure title-author combination has not been processed already; skip if it has
         title_author = f'{title.lower()}_{author.lower()}'
-
         with lock:
             if title_author in processed_books:
                 continue
             processed_books.add(title_author)
 
-        if description and thumbnail:  # Ensure both description and thumbnail exist
+        # Ensure both description and thumbnail exist before adding book data
+        if description and thumbnail:
             books.append({
                 'title': title,
                 'author': author,
@@ -148,4 +216,4 @@ def process_books(books_data, other_metadata, processed_books, lock):
 
 
 if __name__ == '__main__':
-    main()
+    generate_book_data('data/books.json')
--- scripts/upload_to_s3.py
+++ scripts/upload_to_s3.py
@@ -5,12 +5,18 @@ import aiohttp
 import os
 import json
 from dotenv import load_dotenv
-import requests
 from io import BytesIO
 from tqdm.asyncio import tqdm as atqdm
 import time         
 
-async def upload_data():
+async def upload_data(file_path):
+    """
+    Uploads book thumbnails from book JSON file to an AWS S3 bucket asynchronously.
+
+    Args:
+        file_path (str): The path to the JSON file containing book data.
+    """
+
     load_dotenv()
 
     # Retrieve AWS credentials for client
@@ -22,7 +28,7 @@ async def upload_data():
     start = time.time()
 
     # Access book data from file
-    with open('data/books.json', 'r') as file:
+    with open(file_path, 'r') as file:
         books = json.load(file)
 
     # Create asynchronous clients for HTTP req and S3 client
@@ -34,21 +40,24 @@ async def upload_data():
     ) as s3_client:
         num_books = len(books)
 
-        # tasks will hold the coroutines to be executed
+        # Tasks will hold the coroutines to be executed
         tasks = []
 
-        # progress bar
+        # Progress bar
         pbar = atqdm(total=num_books, desc="Uploading Book Thumbnails to S3")
 
-        # setup and start executing coroutines
+        # Setup and execute coroutines
         for book in books:
+
+            # Extract data from JSON file and generate unique S3 key
             title = camel_case(book['title'])
             author = camel_case(book['author'])
             year = book['published_year']
             thumbnail_url = book['thumbnail']
             s3_key = f'{title}_{author}_{year}'
 
-            task = asyncio.create_task(download_and_upload(
+            # Create coroutine task and schedule it for execution
+            task = asyncio.create_task(download_and_upload_thumbnail(
                 session=session,
                 s3_client=s3_client,
                 thumbnail_url=thumbnail_url,
@@ -58,11 +67,12 @@ async def upload_data():
             ))
             tasks.append(task)
 
-        # wait for coroutines to finish and count successes
+        # Wait for coroutines to finish and count successes
         results = await asyncio.gather(*tasks)
         pbar.close()
         num_thumbnails_uploaded = sum(results)
 
+    # Calculate elapsed time
     end = time.time()
     time_elapsed = end - start
     print(f'Time elapsed: {time_elapsed} seconds')
@@ -70,36 +80,59 @@ async def upload_data():
     print(f'Uploaded {num_thumbnails_uploaded}/{num_books} thumbails.')
 
 
-async def download_and_upload(session, s3_client, thumbnail_url, bucket_name, s3_key, pbar):
+async def download_and_upload_thumbnail(session, s3_client, thumbnail_url, bucket_name, s3_key, pbar):
+    """
+    Downloads a thumbnail image from a URL and uploads it to an S3 bucket.
+
+    Args:
+        session (aiohttp.ClientSession): The asynchronous HTTP session.
+        s3_client (aioboto3.S3.Client): The asynchronous S3 client.
+        thumbnail_url (str): The URL of the thumbnail image.
+        bucket_name (str): The name of the S3 bucket.
+        s3_key (str): The S3 key for the uploaded image.
+        pbar (tqdm): The progress bar to update.
+
+    Returns:
+        bool: True if the thumbnail was successfully uploaded, False otherwise.
+    """
     try:
+        # Asynchronously download data in memory, and then upload to S3
         async with session.get(thumbnail_url) as response:
             if response.status == 200:
                 image_data = BytesIO(await response.read())
                 await s3_client.upload_fileobj(image_data, bucket_name, s3_key)
-                pbar.update(1)
+                pbar.update(1) # Update progress bar
                 return True
             else:
                 print(f'Failed to download image from {thumbnail_url}. Status: {response.status}')
-                pbar.update(1)
+                pbar.update(1) # Update progress bar
                 return False
-            
     except aiohttp.ClientError as e:
         print(f'HTTP client error occured: {e}')
-        pbar.update(1)
+        pbar.update(1) # Update progress bar
         return False
     except botocore.exceptions.ClientError as e:
         print(f"An AWS service error occurred: {e}")
-        pbar.update(1)
+        pbar.update(1) # Update progress bar
         return False
     except Exception as e:
         print(f"An error occurred: {e}")
-        pbar.update(1)
+        pbar.update(1) # Update progress bar
         return False
 
 def camel_case(text):
+    """
+    Converts a given string to camel case.
+
+    Args:
+        text (str): The input string to be converted.
+
+    Returns:
+        str: The camel case version of the input string.
+    """
     words = text.split()
     return ''.join([word.capitalize() for word in words])
 
 
 if __name__ == '__main__':
-    asyncio.run(upload_data())
+    asyncio.run(upload_data('data/books.json'))

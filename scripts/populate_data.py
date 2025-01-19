c3a72ef5114ec6946c5eacf61b977fb5e15bda0d
Keetarp <pkapoor1001@gmail.com>
2025-01-19 12:31:55 -0500

    add pinecone task for async upload of book data

--- scripts/populate_data.py
+++ scripts/populate_data.py
@@ -1,25 +1,26 @@
 import asyncio
 from upload_to_s3 import upload_data as upload_to_s3
 from upload_to_mongo import upload_data as upload_to_mongo
+from upload_to_pinecone import upload_data as upload_to_pinecone
 import time
 
 async def populate_data(file_path):
-    # Generate new book data and upload to S3 and MongoDB asynchronously
-    start = time.time()
-
+    # Upload book data to S3, MongoDB, and Pinecone asynchronously
     try:
         upload_to_s3_task = asyncio.create_task(upload_to_s3(file_path))
         upload_to_mongo_task = asyncio.to_thread(upload_to_mongo, file_path)
+        upload_to_pinecone_task = asyncio.to_thread(upload_to_pinecone, file_path)
 
-        await asyncio.gather(upload_to_s3_task, upload_to_mongo_task)
+        await asyncio.gather(upload_to_s3_task, upload_to_mongo_task, upload_to_pinecone_task)
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    end = time.time()
-    elapsed_time = end - start
-    print(f'Total time elapsed: {elapsed_time}')
-    print("Book data populated in S3 and MongoDB successfully!")
+    print("Book data populated in S3 and MongoDB and Pinecone successfully!")
 
 if __name__ == '__main__':
+    start = time.time()
     file_path = 'data/books.json'
     asyncio.run(populate_data(file_path))
+    end = time.time()
+    time_elapsed = end - start
+    print(f'Time elapsed: {time_elapsed} seconds')
